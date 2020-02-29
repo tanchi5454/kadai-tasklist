7e12c02a456714ef3df12ab263401b6fd0e401cf
shinsuke.tano <shinsuke.t+git@hidaka-st-lab.com>
2020-02-29 07:41:55 +0000

    post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_043303) do
+ActiveRecord::Schema.define(version: 2020_02_29_072643) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -25,6 +27,10 @@ ActiveRecord::Schema.define(version: 2020_02_27_043303) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "tasks_id"
+    t.index ["tasks_id"], name: "index_users_on_tasks_id"
   end
 
+  add_foreign_key "tasks", "users"
+  add_foreign_key "users", "tasks", column: "tasks_id"
 end
