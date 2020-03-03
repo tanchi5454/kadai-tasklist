3c301b8f79f8b34bf072f87d02ee0b8beb23ddfc
shinsuke.tano <shinsuke.t+git@hidaka-st-lab.com>
2020-03-03 01:52:00 +0000

    Submission1-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
+  
   def index
 #    @tasks = Task.all.page(params[:page]).per(10)
     if logged_in?
@@ -18,9 +20,6 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
-    puts 'ここを見ろ'
-    p @task
-
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -49,12 +48,13 @@ class TasksController < ApplicationController
   def destroy
 #    @task = Task.find(params[:id])
     @task.destroy
-    flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
 
   private
-
+  
   def set_task
     @task = Task.find(params[:id])
   end
@@ -62,5 +62,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  resources :tasks
-
   root to: 'tasks#index'
   
+  resources :sessions, only: [:new]
+  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks
+
+  get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  
-  resources :tasks, only: [:create, :destroy]
 end
