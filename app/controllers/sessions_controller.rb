08ccecdacad3eeeee54f37b0f56bfee346b44329
shinsuke.tano <shinsuke.t+git@hidaka-st-lab.com>
2020-03-10 08:27:53 +0000

    Submission1-4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = 'ログアウトしました。'
-    redirect_to login_path
+    redirect_to login_url
   end
   
   private
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
@@ -57,7 +57,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+#    @task = Task.find(params[:id])
   end
 
   # Strong Parameter
