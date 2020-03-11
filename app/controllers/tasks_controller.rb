a1e104961ddf3be22823adb23c6f46a219bf0e6c
shinsuke.tano <shinsuke.t+git@hidaka-st-lab.com>
2020-03-11 07:46:42 +0000

    Submission1-6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,15 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-#    @tasks = Task.all.page(params[:page]).per(10)
     if logged_in?
-      @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
   end
 
-#  def show
-#    @task = Task.find(params[:id])
-#  end
+  def show
+  end
 
   def new
     @task = Task.new
@@ -31,12 +27,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-#    @task = Task.find(params[:id])
   end
 
   def update
-#    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -47,7 +40,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-#    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -56,10 +48,6 @@ class TasksController < ApplicationController
 
   private
   
-  def set_task
-#    @task = Task.find(params[:id])
-  end
-
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
